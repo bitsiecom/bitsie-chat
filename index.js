a33078c3ca7fd17a4c56c35aaebb522475e77f63
Josh R Riddle <jriddle@summitglobaltesting.com>
2015-02-15 07:53:28 -0500

    Remove chat console message

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ io.on('connection', function(socket){
 	console.log('a user connected');
 
 	socket.on('chat message', function(msg){
-		console.log('message: ' + msg);
     	io.emit('chat message', msg);
 	});
 
