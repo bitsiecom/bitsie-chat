ab06eeb5898963d1d488d55e2a8febf0c9e0e680
Brent <bhoffmann@summitglobaltesting.com>
2015-02-17 18:23:36 -0500

    initital angular.js integration with socket.io

---
+++ public/js/ChatModule.js
@@ -0,0 +1,31 @@
+var bitsieChat = angular.module('myApp', []);
+bitsieChat.controller('ChatController', ['$scope', function($scope){
+
+	var socket = io();
+	$scope.messages = [];
+	socket.emit('join', roomId);
+	socket.on('chat message', function(user, message) {
+		$scope.$apply(function(){
+			$scope.messages.push({user: user, message: message});
+		});
+	});
+
+	socket.on('update', function(user, message) {	
+		$scope.$apply(function(){
+			$scope.messages.push({user: user, message: message});
+		});
+	});
+
+	socket.on('update people', function(people) {
+		for(var i = 0; i < people.length; i++) {
+			console.log(people[i]);
+		}
+	});
+
+	$('#form-chat').submit(function(e) {
+		e.preventDefault();
+		socket.emit('chat message', $('#m').val());
+		$('#m').val('');
+	});
+
+}]);
--- public/js/app.js
+++ public/js/app.js
@@ -9,32 +9,7 @@ function createRoom()
 	window.location.hash = text;
 	return text;
 }
-
-var socket = io();
 var roomId = window.location.hash.replace("#", "");
 if (!roomId) {
 	roomId = createRoom();
 }
-socket.emit('join', roomId);
-
-socket.on('chat message', function(user, message) {
-	var html = '<strong style="color:' + user.color + '">' + user.name + '</strong>: ' + message;
-	$('#messages').append($('<li>').html(html));
-});
-
-socket.on('update', function(user, message) {
-	var html = '<strong style="color:' + user.color + '">' + user.name + '</strong> ' + message;
-	$('#messages').append($('<li>').html(html));
-});
-
-socket.on('update people', function(people) {
-	for(var i = 0; i < people.length; i++) {
-		console.log(people[i]);
-	}
-});
-
-$('#form-chat').submit(function(e) {
-	e.preventDefault();
-	socket.emit('chat message', $('#m').val());
-	$('#m').val('');
-});
