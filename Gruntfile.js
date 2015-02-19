cd30b5f3e6dcbf625bc972867f51b9608b33b111
Joshua Riddle <josh@bitsie.com>
2015-02-19 10:00:35 -0500

    Added readme and grunt startup script.

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,9 @@ module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		// JS TASKS ================================================================
-		// check all js files for errors
+
 		jshint: {
-			all : ['public/js/**/*.js', '!public/js/aes.js', '!public/js/**/*.min.js'] 
+			default : ['public/js/**/*.js', '!public/js/aes.js', '!public/js/**/*.min.js'] 
 		},
 		
 		karma: {
@@ -16,13 +15,66 @@ module.exports = function(grunt) {
 			unit: {
 				singleRun: false
 			}
-		}
+		},
+
+		bower_concat: {
+			default: {
+				dest: 'public/dist/js/_bower.js',
+				cssDest: 'public/dist/style/_bower.css',
+				dependencies: {
+				},
+				bowerOptions: {
+					relative: false
+				}
+			}
+		},
+
+		watch: {
+			js : {
+				files: ['public/js/**/*.js'],
+				tasks: ['jshint', 'uglify'],
+				options: {
+					spawn: false,
+				}
+			},
+			css : {
+				files: ['public/css/**/*'],
+				tasks: ['sass'],
+				options: {
+					spawn: false,
+				}
+			}
+		},		
+
+		// watch our node server for changes
+		nodemon: {
+			default: {
+				script: 'index.js',
+				options : {
+					ignore : ['coverage/**', 'node_modules/**', 'public/**', '.sass_cache/']
+				}
+				//watch: ['server.js', 'routes/*.js', 'lib/**/*', 'config/**/*']
+			}
+		},
+
+		// run watch and nodemon at the same time
+		concurrent: {
+			options: {
+				logConcurrentOutput: true
+			},
+			tasks: ['nodemon', 'watch']
+		},
 
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-bower-concat');
+	grunt.loadNpmTasks('grunt-nodemon');
+	grunt.loadNpmTasks('grunt-concurrent');
 	grunt.loadNpmTasks('grunt-karma');
 	
+	grunt.registerTask('default', ['jshint', 'concurrent']);
 	grunt.registerTask('test-unit', ['jshint', 'karma']);
 
 };
